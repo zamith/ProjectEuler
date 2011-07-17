301ac6c6ce8440bf2229d9ebc0723826b945ba33
Zamith <zamith.28@gmail.com>
2011-07-17 15:07:34 +0100

    Problem 22

---
+++ 022.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby -w
+
+### Pre processing
+# ruby -a -F, -i -ne 'puts $F' names.txt
+# sort -g names.txt > names_alf.txt
+
+total_names_score=0
+File.open("names_alf.txt", "r").each_line do |line|
+  line.gsub!("\"","").strip!
+  alf_val=0
+  line.upcase.each_char do |char|
+    alf_val_char = char.ord - 64
+    alf_val+=alf_val_char
+  end
+  total_names_score += alf_val * $.
+end
+
+p total_names_score
+
